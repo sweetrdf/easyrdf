52e81c1b4dfa530eb02b2a596be57f5906f7aa0d
Ilias Dimopoulos <idimopoulos@hotmail.com>
2023-04-15 14:58:23 +0300

    Add tests for UTF8-encoded text.

--- test/EasyRdf/Serialiser/NtriplesTest.php
+++ test/EasyRdf/Serialiser/NtriplesTest.php
@@ -335,4 +335,53 @@ class NtriplesTest extends TestCase
 
         $this->assertEquals('"\u00C1\u00C2\u00C3\u00C4\u00C5\u00C6\u00C7\u00C8\u00C9\u00CA\u00CB\u00CC\u00CD\u00CE\u00CF"', $actual);
     }
+
+    /**
+     * Tests the basic Latin characters from U+0020 to U+007E.
+     */
+    public function testVisibleLatinCharacters()
+    {
+        $serializer = new Ntriples();
+        $string = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+        $literal = new Literal($string);
+        $actual = $serializer->serialiseValue($literal);
+        $expected = '" !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"';
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Tests the Latin-1 Supplement characters from U+00A0 to U+00FF.
+     */
+    public function testLatin1SupplementCharacters() {
+        $serializer = new Ntriples();
+        $string = " ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
+        $literal = new Literal($string);
+        $actual = $serializer->serialiseValue($literal);
+        $expected = '" \u00A1\u00A2\u00A3\u00A4\u00A5\u00A6\u00A7\u00A8\u00A9\u00AA\u00AB\u00AC\u00AD\u00AE\u00AF\u00B0\u00B1\u00B2\u00B3\u00B4\u00B5\u00B6\u00B7\u00B8\u00B9\u00BA\u00BB\u00BC\u00BD\u00BE\u00BF\u00C0\u00C1\u00C2\u00C3\u00C4\u00C5\u00C6\u00C7\u00C8\u00C9\u00CA\u00CB\u00CC\u00CD\u00CE\u00CF\u00D0\u00D1\u00D2\u00D3\u00D4\u00D5\u00D6\u00D7\u00D8\u00D9\u00DA\u00DB\u00DC\u00DD\u00DE\u00DF\u00E0\u00E1\u00E2\u00E3\u00E4\u00E5\u00E6\u00E7\u00E8\u00E9\u00EA\u00EB\u00EC\u00ED\u00EE\u00EF\u00F0\u00F1\u00F2\u00F3\u00F4\u00F5\u00F6\u00F7\u00F8\u00F9\u00FA\u00FB\u00FC\u00FD\u00FE\u00FF"';
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Tests the Latin Extended-A characters from U+0100 to U+017F.
+     */
+    public function testLatinExtendedACharacters()
+    {
+        $serializer = new Ntriples();
+        $string = " ĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſ";
+        $literal = new Literal($string);
+        $actual = $serializer->serialiseValue($literal);
+        $expected = '" \u0100\u0101\u0102\u0103\u0104\u0105\u0106\u0107\u0108'.
+            '\u0109\u010A\u010B\u010C\u010D\u010E\u010F\u0110\u0111\u0112\u0113'.
+            '\u0114\u0115\u0116\u0117\u0118\u0119\u011A\u011B\u011C\u011D\u011E'.
+            '\u011F\u0120\u0121\u0122\u0123\u0124\u0125\u0126\u0127\u0128\u0129'.
+            '\u012A\u012B\u012C\u012D\u012E\u012F\u0130\u0131\u0132\u0133\u0134'.
+            '\u0135\u0136\u0137\u0138\u0139\u013A\u013B\u013C\u013D\u013E\u013F'.
+            '\u0140\u0141\u0142\u0143\u0144\u0145\u0146\u0147\u0148\u0149\u014A'.
+            '\u014B\u014C\u014D\u014E\u014F\u0150\u0151\u0152\u0153\u0154\u0155'.
+            '\u0156\u0157\u0158\u0159\u015A\u015B\u015C\u015D\u015E\u015F\u0160'.
+            '\u0161\u0162\u0163\u0164\u0165\u0166\u0167\u0168\u0169\u016A\u016B'.
+            '\u016C\u016D\u016E\u016F\u0170\u0171\u0172\u0173\u0174\u0175\u0176'.
+            '\u0177\u0178\u0179\u017A\u017B\u017C\u017D\u017E\u017F"';
+        $this->assertEquals($expected, $actual);
+    }
 }
