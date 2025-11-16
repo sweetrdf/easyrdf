d0c646093616ad4290be8ac53b688f3bc2698bcf
Konrad Abicht <hi@inspirito.de>
2025-11-16 12:59:02 +0100

    translated German texts

--- docker/run.sh
+++ docker/run.sh
@@ -3,11 +3,11 @@
 cd /home/easyrdf/fuseki && ./fuseki-server --port=3030 --update --loc /home/easyrdf/fuseki/databases/DB /ds &
 
 until nc -z localhost 3030; do
-  echo "Warten auf Fuseki..."
+  echo "Waiting on Fuseki..."
   sleep 2
 done
 
-echo "Fuseki läuft im Hintergrund (PID $!). Starten der interaktiven Shell..."
+echo "Fuseki runs in the background (PID $!). Starting interactive shell ..."
 
 # Starten Sie die Bash im Vordergrund. Der Container bleibt aktiv, solange die Bash läuft.
 exec /bin/bash
